e2ea473e26c122ca068e6b4ec7c0ece1aea1cbf2
6804062612145_ชญาภา <s6804062612145@email.kmutnb.ac.th>
2026-03-02 21:09:51 +0700

    lab10no3_1.c

---
+++ lab10_3.1.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+struct Books{ 
+	int BookID; 
+	char BookTitle[50]; 
+} ComputerBook; 
+void DisplayData(struct Books CBook); 
+
+int main() { 
+	printf("Enter Book ID : ");      
+	scanf("%d",&ComputerBook.BookID); 
+	printf("Enter Book Title : ");   
+	scanf("%s",ComputerBook.BookTitle); 
+	DisplayData(ComputerBook);  
+	return 0; 
+} 
+void DisplayData(struct Books CBook){ 
+	printf("Book ID : %d \n",CBook.BookID); 
+	printf("Book Title : %s \n",CBook.BookTitle); 
+} 
+
+//void DisplayData(struct Books CBook) is function that show BookID and BookTitle.
+
+//passing parameter is BookID and BookTitle in struct book.
